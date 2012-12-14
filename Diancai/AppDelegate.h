2329e49d62f9d5b2ab004d8202c099cf2a9ee442
s3209_25 <s3209_25@s3209-25matoiMac.local>
2012-12-14 16:03:27 +0800

    Initial Commit

---
+++ Diancai/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Diancai
+//
+//  Created by s3209_25 on 12-12-14.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
